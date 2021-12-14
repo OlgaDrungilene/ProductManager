672193b445182f3575fd13658845d4417482d290
Olga Drungilene <olgadrungilene@hotmail.com>
2021-12-14 17:54:23 +0100

    Deleted Zip files

---
+++ Data/ProductManagerContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManager.Data
+{
+    class ProductManagerContext: DbContext
+    {
+        private readonly string connectionString;
+
+        public DbSet<Product> Products { get; set; }
+        public ProductManagerContext (string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+         
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer (connectionString); 
+        }
+    }
+}
